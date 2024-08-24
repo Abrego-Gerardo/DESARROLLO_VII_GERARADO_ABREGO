0d28f2796172628f83c583838d2cf6f2957d3a52
Abrego-Gerardo <143464224+Abrego-Gerardo@users.noreply.github.com>
2024-08-24 00:29:50 -0500

    Update index.php

--- TALLER_2/index.php
+++ TALLER_2/index.php
@@ -1,11 +1,28 @@
 <?php
-$nombre = "Juan ";
-$edad = 25;
-$altura = 1.75;
-$esEstudiante = true;
+// Definición de variables
+$nombre = "María";
+$edad = 30;
+$ciudad = "Madrid";
 
-echo "Nombre: $nombre<br>";
-echo "Edad: $edad<br>";
-echo "Altura: $altura<br>";
-echo "¿Es estudiante? " . ($esEstudiante ? "Sí" : "No");
+// Definición de constante
+define("PROFESION", "Ingeniera");
+
+// Creación de mensaje usando diferentes métodos de concatenación e impresión
+$mensaje1 = "Hola, mi nombre es " . $nombre . " y tengo " . $edad . " años.";
+$mensaje2 = "Vivo en $ciudad y soy " . PROFESION . ".";
+
+echo $mensaje1 . "<br>";
+print($mensaje2 . "<br>");
+
+printf("En resumen: %s, %d años, %s, %s<br>", $nombre, $edad, $ciudad, PROFESION);
+
+echo "<br>Información de debugging:<br>";
+var_dump($nombre);
+echo "<br>";
+var_dump($edad);
+echo "<br>";
+var_dump($ciudad);
+echo "<br>";
+var_dump(PROFESION);
+echo "<br>";
 ?>
