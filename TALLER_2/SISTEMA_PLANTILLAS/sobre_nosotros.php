aa0b61be305769b5223d24e0c11eed4832bf21d4
Abrego-Gerardo <143464224+Abrego-Gerardo@users.noreply.github.com>
2024-08-24 20:59:56 -0500

    Update sobre_nosotros.php

--- TALLER_2/SISTEMA_PLANTILLAS/sobre_nosotros.php
+++ TALLER_2/SISTEMA_PLANTILLAS/sobre_nosotros.php
@@ -1,15 +1,13 @@
 <?php
-$paginaActual = 'inicio'; // Cambia esto según el archivo
+$paginaActual = 'sobre_nosotros';
 require_once 'plantillas/funciones.php';
 $tituloPagina = obtenerTituloPagina($paginaActual);
 include 'plantillas/encabezado.php';
 ?>
 
-<h2>Contenido de la Página de Inicio</h2>
-<p>Este es el contenido específico de la página de inicio.</p>
+<h2>Contenido de la Página de Sobre Nosotros</h2>
+<p>Este es el contenido específico de la página Sobre Nosotros.</p>
 
 <?php
 include 'plantillas/pie_pagina.php';
 ?>
-
-http://localhost/TALLERES/TALLER_2/SISTEMA_PLANTILLAS/sobre_nosotros.php
