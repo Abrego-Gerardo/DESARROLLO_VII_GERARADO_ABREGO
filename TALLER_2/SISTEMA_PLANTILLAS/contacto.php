6dfcfbda481e98080a9c51ace2f9f7a7d63f3dcd
Abrego-Gerardo <143464224+Abrego-Gerardo@users.noreply.github.com>
2024-08-24 21:00:19 -0500

    Update contacto.php

--- TALLER_2/SISTEMA_PLANTILLAS/contacto.php
+++ TALLER_2/SISTEMA_PLANTILLAS/contacto.php
@@ -1,15 +1,13 @@
 <?php
-$paginaActual = 'inicio'; // Cambia esto según el archivo
+$paginaActual = 'contacto';
 require_once 'plantillas/funciones.php';
 $tituloPagina = obtenerTituloPagina($paginaActual);
 include 'plantillas/encabezado.php';
 ?>
 
-<h2>Contenido de la Página de Inicio</h2>
-<p>Este es el contenido específico de la página de inicio.</p>
+<h2>Contenido de la Página de Contacto</h2>
+<p>Este es el contenido específico de la página de Contacto.</p>
 
 <?php
 include 'plantillas/pie_pagina.php';
 ?>
-
-http://localhost/TALLERES/TALLER_2/SISTEMA_PLANTILLAS/contacto.php
